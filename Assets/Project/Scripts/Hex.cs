bb11a83156537d6a89b4f48dce19f12e676f29d4
Lutfullah Turker <lutfullahturker@gmail.com>
2021-03-08 13:48:12 +0300

    Hexgroup List access methods implemented. GetCenterPositionOfGroup method implemented.

--- Assets/Project/Scripts/Hex.cs
+++ Assets/Project/Scripts/Hex.cs
@@ -7,12 +7,12 @@ namespace Project.Scripts
     public class Hex : MonoBehaviour
     {
         [HideInInspector] public Vector2Int tablePos;
-
         [HideInInspector] public Color color;
-
         public Dictionary<NeighbourDirection, Vector2Int> neighbours;
 
         private List<HexGroup> _allHexGroups;
+        private int selectedHexGroupIndex;
+        private bool isFirstAccessToHexGroupList = true;
 
         // Start is called before the first frame update
         void Start()
@@ -24,11 +24,39 @@ namespace Project.Scripts
         {
         }
 
-        public List<HexGroup> GetAllPossibleHexGroups()
+        public HexGroup GetNextHexGroup()
+        {
+            GetAllPossibleHexGroups();
+            if (selectedHexGroupIndex == _allHexGroups.Count - 1 || isFirstAccessToHexGroupList)
+            {
+                isFirstAccessToHexGroupList = false;
+                selectedHexGroupIndex = 0;
+            }
+            else
+            {
+                selectedHexGroupIndex += 1;
+            }
+
+            return _allHexGroups[selectedHexGroupIndex];
+        }
+
+        public HexGroup GetSelectedHexGroup()
+        {
+            GetAllPossibleHexGroups();
+            return _allHexGroups[selectedHexGroupIndex];
+        }
+
+        public void ResetSelectedHexGroup()
+        {
+            selectedHexGroupIndex = 0;
+            isFirstAccessToHexGroupList = true;
+        }
+        
+        private void GetAllPossibleHexGroups()
         {
             if (_allHexGroups != null)
             {
-                return _allHexGroups;
+                return;
             }
 
             _allHexGroups = new List<HexGroup>();
@@ -37,40 +65,38 @@ namespace Project.Scripts
             if (IsHexGroupValid(keyList, NeighbourDirection.TOP, NeighbourDirection.LEFT_TOP))
             {
                 _allHexGroups.Add(new HexGroup(tablePos, neighbours[NeighbourDirection.TOP],
-                    neighbours[NeighbourDirection.LEFT_TOP]));
+                    neighbours[NeighbourDirection.LEFT_TOP], HexGroup.Rotation.RightTwo));
             }
 
             if (IsHexGroupValid(keyList, NeighbourDirection.LEFT_TOP, NeighbourDirection.LEFT_DOWN))
             {
                 _allHexGroups.Add(new HexGroup(tablePos, neighbours[NeighbourDirection.LEFT_TOP],
-                    neighbours[NeighbourDirection.LEFT_DOWN]));
+                    neighbours[NeighbourDirection.LEFT_DOWN], HexGroup.Rotation.LeftTwo));
             }
 
             if (IsHexGroupValid(keyList, NeighbourDirection.LEFT_DOWN, NeighbourDirection.DOWN))
             {
                 _allHexGroups.Add(new HexGroup(tablePos, neighbours[NeighbourDirection.LEFT_DOWN],
-                    neighbours[NeighbourDirection.DOWN]));
+                    neighbours[NeighbourDirection.DOWN], HexGroup.Rotation.RightTwo));
             }
 
             if (IsHexGroupValid(keyList, NeighbourDirection.DOWN, NeighbourDirection.RIGHT_DOWN))
             {
                 _allHexGroups.Add(new HexGroup(tablePos, neighbours[NeighbourDirection.DOWN],
-                    neighbours[NeighbourDirection.RIGHT_DOWN]));
+                    neighbours[NeighbourDirection.RIGHT_DOWN], HexGroup.Rotation.LeftTwo));
             }
 
             if (IsHexGroupValid(keyList, NeighbourDirection.RIGHT_DOWN, NeighbourDirection.RIGHT_TOP))
             {
                 _allHexGroups.Add(new HexGroup(tablePos, neighbours[NeighbourDirection.RIGHT_DOWN],
-                    neighbours[NeighbourDirection.RIGHT_TOP]));
+                    neighbours[NeighbourDirection.RIGHT_TOP], HexGroup.Rotation.RightTwo));
             }
 
             if (IsHexGroupValid(keyList, NeighbourDirection.RIGHT_TOP, NeighbourDirection.TOP))
             {
                 _allHexGroups.Add(new HexGroup(tablePos, neighbours[NeighbourDirection.RIGHT_TOP],
-                    neighbours[NeighbourDirection.TOP]));
+                    neighbours[NeighbourDirection.TOP], HexGroup.Rotation.LeftTwo));
             }
-
-            return _allHexGroups;
         }
 
         private bool IsHexGroupValid(
--- Assets/Project/Scripts/HexGroup.cs
+++ Assets/Project/Scripts/HexGroup.cs
@@ -7,12 +7,42 @@ namespace Project.Scripts
         public Vector2Int mainHex;
         public Vector2Int firstHex;
         public Vector2Int secondHex;
+        public Rotation rotation;
         
-        public HexGroup(Vector2Int _mainHex, Vector2Int _firstHex, Vector2Int _secondHex)
+        public enum RotateDirection
         {
+            Clockwise,
+            CounterClockwise
+        }
+
+        public enum Rotation
+        {
+            LeftTwo,
+            RightTwo
+        }
+        
+        public HexGroup(Vector2Int _mainHex, Vector2Int _firstHex, Vector2Int _secondHex, Rotation _rotation)
+        {
+            rotation = _rotation;
             mainHex = _mainHex;
             firstHex = _firstHex;
             secondHex = _secondHex;
+        }
+
+        public Vector3 GetCenterPositionOfGroup()
+        {
+            var mainHexPosition = GridCreator.gameTable[mainHex.x][mainHex.y].transform.position;
+            var firstHexPosition = GridCreator.gameTable[firstHex.x][firstHex.y].transform.position;
+            var secondHexPosition = GridCreator.gameTable[secondHex.x][secondHex.y].transform.position;
+
+            var result = Vector3.zero;
+            result += mainHexPosition;
+            result += firstHexPosition;
+            result += secondHexPosition;
+            
+            // Debug.Log(result/3.0f);
+
+            return result / 3;
         }
     }
 }
