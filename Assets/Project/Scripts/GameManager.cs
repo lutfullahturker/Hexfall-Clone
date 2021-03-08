cc1e521793345b2dc78355c7b5c813971cd4aa36
Lutfullah Turker <lutfullahturker@gmail.com>
2021-03-08 15:43:48 +0300

    Rotate direction calculation method implemented and used when rotate needs.

--- Assets/Project/Scripts/GameManager.cs
+++ Assets/Project/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System;
-using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
 
@@ -10,10 +11,14 @@ namespace Project.Scripts
         [SerializeField] private GameObject highlightGameObject;
 
         private Vector2Int? _selectedHexPosition;
+        private HexGroup _selectedHexGroup = null;
+        private bool _isHexGroupRotating = false;
+        private List<List<GameObject>> gameTable;
 
         // Start is called before the first frame update
         void Start()
         {
+            gameTable = GridCreator.gameTable;
             EventManager.Current.onHexagonTouched += OnTouchedHexagon;
             EventManager.Current.onSwiped += OnSwiped;
         }
@@ -23,19 +28,57 @@ namespace Project.Scripts
         {
         }
 
-        private void OnDestroy()
+        void OnDestroy()
         {
             EventManager.Current.onHexagonTouched -= OnTouchedHexagon;
             EventManager.Current.onSwiped -= OnSwiped;
         }
+
+        private IEnumerator RotateCoroutine(InputManager.SwipeDirection swipeDirection, Vector2 swipePos)
+        {
+            _isHexGroupRotating = true;
+
+            var rotateDirection =
+                GetRotateDirection(swipeDirection, _selectedHexGroup.GetCenterPositionOfGroup(), swipePos);
+
+            if (_selectedHexPosition.HasValue)
+            {
+                yield return StartCoroutine(gameTable[_selectedHexPosition.Value.x][_selectedHexPosition.Value.y]
+                    .GetComponent<Hex>()
+                    .Rotate(rotateDirection));
+            }
+
+            _isHexGroupRotating = false;
+        }
+
+        private HexGroup.RotateDirection GetRotateDirection(
+            InputManager.SwipeDirection swipeDirection,
+            Vector2 centerPosOfGroup,
+            Vector2 swipePos)
+        {
+            var isSwipeAboveCenter = swipePos.y > centerPosOfGroup.y;
+
+            return swipeDirection switch
+            {
+                InputManager.SwipeDirection.LeftSwipe => isSwipeAboveCenter
+                    ? HexGroup.RotateDirection.CounterClockwise
+                    : HexGroup.RotateDirection.Clockwise,
+                InputManager.SwipeDirection.RightSwipe => isSwipeAboveCenter
+                    ? HexGroup.RotateDirection.Clockwise
+                    : HexGroup.RotateDirection.CounterClockwise,
+                _ => throw new ArgumentOutOfRangeException(nameof(swipeDirection), swipeDirection, null)
+            };
+        }
+
+        #region Event Callbacks
 
         private void OnTouchedHexagon(GameObject touchedHex)
         {
             // Debug.Log("Touched GO: " + touchedHex.transform.name);
 
             var hexComponent = touchedHex.GetComponent<Hex>();
-            var hexGroup = hexComponent.GetNextHexGroup();
-            var centerPosition = hexGroup.GetCenterPositionOfGroup();
+            _selectedHexGroup = hexComponent.GetNextHexGroup();
+            var centerPosition = _selectedHexGroup.GetCenterPositionOfGroup();
 
             if (_selectedHexPosition != null && hexComponent.tablePos != _selectedHexPosition)
             {
@@ -45,7 +88,7 @@ namespace Project.Scripts
 
             _selectedHexPosition = hexComponent.tablePos;
             highlightGameObject.SetActive(true);
-            if (hexGroup.rotation == HexGroup.Rotation.LeftTwo)
+            if (_selectedHexGroup.highlightSpriteRotation == HexGroup.HighlightSpriteRotation.LeftTwo)
             {
                 highlightGameObject.transform.DORotate(new Vector3(0f, 0), 0);
                 centerPosition += new Vector3(0.138f, 0, 0);
@@ -61,7 +104,13 @@ namespace Project.Scripts
 
         private void OnSwiped(InputManager.SwipeDirection swipeDirection, Vector2 swipePosition)
         {
-            
+            if (_isHexGroupRotating || _selectedHexGroup == null || !_selectedHexPosition.HasValue)
+            {
+                return;
+            }
+            StartCoroutine(RotateCoroutine(swipeDirection,swipePosition));
         }
+
+        #endregion
     }
 }
